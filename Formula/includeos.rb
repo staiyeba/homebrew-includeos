30517283d72ffbb97ae243e83e8daa18d9ab7fd8
taiyeba <staiyeba@gmail.com>
2019-04-12 15:14:11 +0200

    tesing cache-4: removed conan as a dependency

--- Formula/includeos.rb
+++ Formula/includeos.rb
@@ -10,7 +10,6 @@ class Includeos < Formula
 	depends_on "qemu"
 	depends_on "cmake"
 	depends_on "python"
-	depends_on "conan"
 
 	resource "psutil" do
 		url "https://files.pythonhosted.org/packages/2f/b8/11ec5006d2ec2998cb68349b8d1317c24c284cf918ecd6729739388e4c56/psutil-5.6.1.tar.gz"
